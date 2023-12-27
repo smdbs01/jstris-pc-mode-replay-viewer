23b1131fdd771fc467ee20a8c12be4245aa64757
smdbs <1289707349@qq.com>
2023-12-27 17:34:22 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    
+
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ uno.config.js
@@ -0,0 +1,34 @@
+// uno.config.ts
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetWind,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
+      // ...
+    }
+  },
+  presets: [
+    presetWind(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        // ...
+      }
+    })
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()]
+})
