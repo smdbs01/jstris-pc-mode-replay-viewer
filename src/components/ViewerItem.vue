ab10dbc4c5ede72911074380686fc540d7fcd686
smdbs <1289707349@qq.com>
2023-12-30 17:11:23 -0500

    Added hover hint for the buttons

--- src/components/ViewerItem.vue
+++ src/components/ViewerItem.vue
@@ -9,35 +9,35 @@
     <footer class="w-[90%] h-[5%] mx-4 my-4 p-2 flex items-center justify-between absolute bottom-0">
       <div class="flex">
         <!-- Reset -->
-        <ButtonIcon class="mr-2 w-6 h-6 md:w-8 md:h-8" @click="reset()" :title="'Reset'">
+        <ButtonIcon class="mr-2 w-6 h-6 sm:w-8 sm:h-8" @click="reset()" :title="'Reset'">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
             <path fill="currentColor"
-              d="M223.77 58a16 16 0 0 0-16.25.53L128 109.14v-37.3a15.91 15.91 0 0 0-24.48-13.34l-88.19 56.16a15.8 15.8 0 0 0 0 26.68l88.19 56.16A15.91 15.91 0 0 0 128 184.16v-37.3l79.52 50.64A15.91 15.91 0 0 0 232 184.16V71.84A15.83 15.83 0 0 0 223.77 58M112 183.93L24.18 128L112 72.06Zm104 0L128.18 128L216 72.06Z" />
+              d="M224 128a96 96 0 0 1-94.71 96H128a95.38 95.38 0 0 1-65.9-26.2a8 8 0 0 1 11-11.63a80 80 0 1 0-1.67-114.78a3.07 3.07 0 0 1-.26.25l-10.54 9.65l17 17A8 8 0 0 1 72 112H24a8 8 0 0 1-8-8V56a8 8 0 0 1 13.66-5.7L49.31 70l10.94-10A96 96 0 0 1 224 128" />
           </svg>
         </ButtonIcon>
         <!-- BackPC -->
-        <ButtonIcon class="mr-2 w-6 h-6 md:w-8 md:h-8" @click="backPC()" :title="'Last PC'">
+        <ButtonIcon class="mr-2 w-6 h-6 sm:w-8 sm:h-8" @click="backPC()" :title="'Last PC'">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
             <path fill="currentColor"
               d="M199.81 34a16 16 0 0 0-16.24.43L64 109.23V40a8 8 0 0 0-16 0v176a8 8 0 0 0 16 0v-69.23l119.57 74.78A15.95 15.95 0 0 0 208 208.12V47.88A15.86 15.86 0 0 0 199.81 34M192 208L64.16 128L192 48.07Z" />
           </svg>
         </ButtonIcon>
         <!-- BackPiece -->
-        <ButtonIcon class="mr-2 w-6 h-6 md:w-8 md:h-8" @click="backPiece()" :title="'Last Piece'">
+        <ButtonIcon class="mr-2 w-6 h-6 sm:w-8 sm:h-8" @click="backPiece()" :title="'Last Piece'">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
             <path fill="currentColor"
               d="M224 128a8 8 0 0 1-8 8H59.31l58.35 58.34a8 8 0 0 1-11.32 11.32l-72-72a8 8 0 0 1 0-11.32l72-72a8 8 0 0 1 11.32 11.32L59.31 120H216a8 8 0 0 1 8 8" />
           </svg>
         </ButtonIcon>
         <!-- ForwardPiece -->
-        <ButtonIcon class="mr-2 w-6 h-6 md:w-8 md:h-8" @click="forwardPiece()" :title="'Next Piece'">
+        <ButtonIcon class="mr-2 w-6 h-6 sm:w-8 sm:h-8" @click="forwardPiece()" :title="'Next Piece'">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" class="inline-block">
             <path fill="currentColor"
               d="m221.66 133.66l-72 72a8 8 0 0 1-11.32-11.32L196.69 136H40a8 8 0 0 1 0-16h156.69l-58.35-58.34a8 8 0 0 1 11.32-11.32l72 72a8 8 0 0 1 0 11.32" />
           </svg>
         </ButtonIcon>
         <!-- ForwardPC -->
-        <ButtonIcon class="mr-2 w-6 h-6 md:w-8 md:h-8" @click="forwardPC()" :title="'Next PC'">
+        <ButtonIcon class="mr-2 w-6 h-6 sm:w-8 sm:h-8" @click="forwardPC()" :title="'Next PC'">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" class="inline-block">
             <path fill="currentColor"
               d="M200 32a8 8 0 0 0-8 8v69.23L72.43 34.45A15.95 15.95 0 0 0 48 47.88v160.24a16 16 0 0 0 24.43 13.43L192 146.77V216a8 8 0 0 0 16 0V40a8 8 0 0 0-8-8M64 207.93V48.05l127.84 80Z" />
@@ -45,8 +45,8 @@
         </ButtonIcon>
       </div>
 
-      <div class="flex items-center text-teal-600">
-        <span class="font-semibold mr-2 hidden sm:inline select-none">{{ currentPage[1] }}p <span
+      <div class="flex items-center text-teal-600 text-sm sm:text-base">
+        <span class="font-semibold mr-2 select-none">{{ currentPage[1] }}p <span
             class="font-normal">@</span></span>
         <input type="number"
           class="w-8 inline-block text-center rounded border-1 shadow-teal-600 bg-gray-600 text-teal-200 focus:outline-none hover:bg-gray-500 focus:bg-gray-300 focus:text-gray-700 hover:shadow-gray-300 focus:shadow-gray-300 transition-color"
@@ -182,5 +182,4 @@ input::-webkit-inner-spin-button {
 
 input[type=number] {
   -moz-appearance: textfield;
-}
-</style>
+}</style>
--- src/components/icons/ButtonIcon.vue
+++ src/components/icons/ButtonIcon.vue
@@ -1,9 +1,17 @@
 <template>
-  <div tabindex="0" :title="title"
-    class="relative text-teal-600 hover:text-teal-700 rounded-md hover:cursor-pointer outline-none focus:text-teal-300 active:text-teal-400 active:border-0 transition-all duration-100">
+  <div tabindex="0"
+    class="group relative text-teal-600 hover:text-teal-700 rounded-md hover:cursor-pointer outline-none focus:text-teal-300 active:text-teal-400 active:border-0 transition-all duration-100">
     <slot>
       <!-- A svg here -->
     </slot>
+    <div
+      class="w-[400%] absolute bottom-[110%] -left-[150%] flex items-center justify-center
+      invisible group-hover:visible">
+      <span class="px-2 outline-1 text-teal-300 outline-white bg-gray-600 rounded text-sm md:text-base font-mono tracking-tighter select-none
+      opacity-0 group-hover:opacity-100 transition-all">
+        {{ title }}
+      </span>
+    </div>
   </div>
 </template>
 
